e849af076921309d9599fdb7dc7c129defc00a77
dasFeuer <barunpanthisharma11@gmail.com>
2024-04-03 07:22:56 +0545

    New files added

---
+++ destop_notification.py
@@ -0,0 +1,31 @@
+import requests
+import xml.etree.ElementTree as ET
+RSS_FEED_URL = "https://www.dw.com/de/themen/s-9077"
+
+def load_site():
+    respond = requests.get(RSS_FEED_URL)
+    return respond.content
+
+def parseXML (rss):
+    root = ET.fromstring(rss)
+    newsitems = []
+    for item in root.findall('./channel/item'):
+        news = {}
+
+        for child in item:
+
+            if child.tag == '{https://www.google.com/search?q=google&rlz=1C1KNTJ_enNP1065NP1066&oq=google&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTISCAEQLhhDGMcBGNEDGIAEGIoFMg4IAhBFGCcYOxiABBiKBTIGCAMQRRhBMgYIBBBFGDwyBggFEEUYQTIGCAYQRRg8MgYIBxBFGDzSAQg1MTc3ajBqN6gCCLACAQ&sourceid=chrome&ie=UTF-8}content':
+                news['media'] = child.attrib['url']
+            else: 
+                news[child.tag] = child.text.encode('utf8') 
+        newsitems.append(news) 
+def topStories(): 
+
+    rss = load_site() 
+
+    newsitems = parseXML(rss) 
+    return newsitems 
+
+
+
+
---
+++ screenshot.py
@@ -0,0 +1,10 @@
+import pyscreenshot
+
+image = pyscreenshot.grab() # It capture the screen
+
+image.show() # It dispaly the captured screenshot
+
+image.save('test1.png') # It save the screenshot
+ 
+
+
